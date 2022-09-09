951d35619e449c111cb077b0899a3114eca4fb87
Ciro Farias <83941927+pirius12@users.noreply.github.com>
2022-09-08 22:55:54 -0300

    Add files via upload

---
+++ ciro/index.js
@@ -0,0 +1,11 @@
+
+  let entrada = prompt("Ingrese su edad");
+
+  while(entrada < 18 ){
+    alert("No puedes acceder");
+  
+    entrada = prompt("Ingrese su edad");
+  }
+  
+  
+  alert("Bienvenido");
